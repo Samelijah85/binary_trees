6aaf9b227957221486514790c218f8213e06ed0b
Evanie <evaniewares@gmail.com>
2024-02-28 15:20:50 +0000

    binary trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -27,8 +27,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	}
 
-	left_height = binary_tree_height(tree->left) + 1;
-	right_height = binary_tree_height(tree->right) + 1;
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	return (max(left_height, right_height));
+	return (max(left_height, right_height) + 1);
 }
