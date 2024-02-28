4ea26c28a758522378742399b65cbd64bff67d19
Evanie <evaniewares@gmail.com>
2024-02-28 15:25:48 +0000

    fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -20,15 +20,21 @@ size_t max(size_t a, size_t b)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	if (tree->left)
+	{
+		left_height = binary_tree_height(tree->left) + 1;
+	}
+	if (tree->right)
+	{
+		right_height = binary_tree_height(tree->right) + 1;
+	}
 
-	return (max(left_height, right_height) + 1);
+	return (max(left_height, right_height));
 }
